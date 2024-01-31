0e01574d6e963ea72a84ca7eb12139b307751968
Juanchavez93 <juanchavez1191993@gmail.com>
2024-01-31 02:08:47 -0700

    deploy backend url

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
 
     onSubmitSignIn = () => {
         console.log(this.state)
-        fetch('http://localhost:3000/register', {
+        fetch('https://mybackend1-nubo.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
